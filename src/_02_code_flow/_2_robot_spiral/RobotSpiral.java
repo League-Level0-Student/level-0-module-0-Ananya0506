cb32ec382da84b85d27343ee56572d1860333dc8
league <league@wts14-3.attlocal.net>
2022-09-09 18:28:52 -0700

    i finished my robot spiral, which was good. i also made a pentagon in penatgoncrazy, which looked really cool! lastly, i did my flaming ninja star and added some changes, and it turned out pretty nice.

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -16,26 +16,26 @@ public class RobotSpiral {
 		// Set your robot's pen down 
 		dog.penDown();   
 		// SPEED. Set the robot to go at max speed (100)
-		 dog.setSpeed(100); 
+		 dog.setSpeed(50); 
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
 			//        Start its value as zero.
 		 int count = 0;   
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
-
+          while(count<100) {
 			// COLOR.   Have the robot set a random pen color:      
-		 dog.setRandomPenColor();
+        	  dog.setRandomPenColor();
 			// DRAW.    Move the robot (5*count) pixels
 			//          count is the name of the variable you created earlier
-
+               dog.move(5*count);
 			// TURN.    Turn the robot (360/7) degrees to the right
-
+               dog.turn(360/7);
 			// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
-
+                      count+=1;
 			// Change the robot pen width to the current value of the count variable 
-
+               dog.setPenWidth(count+=1);
 
 		//  End the while loop here
-
+          }
 		
 		// Run the program. 
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
